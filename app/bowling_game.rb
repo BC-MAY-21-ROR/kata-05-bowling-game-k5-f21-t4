f05d992ab60ec8431b27f695c33c64673c6b6a5f
Frcomparan <frcomparan@gmail.com>
2022-02-24 17:49:27 -0600

    Re-factor methods for reduce the numbers of lines
    Co-Authored-By: Alex Silva <alexs1010@gmail.com>
    Co-Authored-By: FranH20 <carloshuichi@gmail.com>

--- app/bowling_game.rb
+++ app/bowling_game.rb
@@ -2,36 +2,22 @@
 
 # Class to start the bowling game
 class BowlingGame
-  attr_accessor :board, :result
+  attr_reader :board, :result
 
-  def initialize
-    @board = Array.new(10) { Array.new(2) { 0 } }
+  def initialize(board = Array.new(10) { Array.new(2) { 0 } })
+    @board = board
     @result = Array.new(10) { 0 }
-    @board[9][2] = 0
+    @board[9][2] = 0 if @board[9][2].nil?
   end
 
   def game
-    @board.size.times do |frame|
-      2.times do |turn|
-        record_knock_down(frame, turn, make_hit(frame, turn))
-        last_frame(frame) if frame == 9
-      end
-      record_per_frame if frame < 10
-    end
+    @board.size.times { |frame| 2.times { |turn| count_record_per_turn(frame, turn) } }
   end
 
-  def record_per_frame
-    10.times do |frame|
-      @result[frame] = @board[frame][0] + @board[frame][1]
-      @result[frame] += @result[frame - 1] if frame.positive?
-      (@result[frame] += @board[frame + 1][0] if spare?(frame)) if frame < 9
-      (@result[frame] += (@board[frame + 1][0] + @board[frame + 1][1]) if strike?(frame)) if frame < 9
-    end
-    @result[9] += @board[9][2]
-  end
-
-  def show_results
-    puts @result.inspect
+  def count_record_per_turn(frame, turn)
+    record_knock_down(frame, turn, make_hit(frame, turn))
+    last_frame(frame) if frame == 9
+    update_results if frame < 10
   end
 
   def record_knock_down(frame, turn, pins_knock_down)
@@ -42,23 +28,36 @@ class BowlingGame
     (record_knock_down(frame, 2, rand(0..10)) if spare?(frame) || strike?(frame))
   end
 
+  def update_results
+    10.times { |frame| record_per_frame(frame) }
+    @result[9] += @board[9][2]
+  end
+
+  def record_per_frame(frame)
+    @result[frame] = @board[frame][0] + @board[frame][1]
+    @result[frame] += @result[frame - 1] if frame.positive?
+    extra_point?(frame) if frame < 9
+  end
+
+  def extra_point?(frame)
+    @result[frame] += @board[frame + 1][0] if spare?(frame)
+    @result[frame] += (@board[frame + 1][0] + @board[frame + 1][1]) if strike?(frame)
+  end
+
+  def show_results
+    puts @result.inspect
+  end
+
   def make_hit(frame, turn)
     turn.zero? ? rand(0..10) : rand(0..(10 - @board[frame][0]))
   end
 
   def spare?(frame)
-    turn1 = @board[frame][0]
-    true if turn1 + @board[frame][1] == 10 && turn1 != 10
+    turn_one = @board[frame][0]
+    true if turn_one + @board[frame][1] == 10 && turn_one != 10
   end
 
   def strike?(frame)
     true if @board[frame][0] == 10
   end
 end
-
-# game.board = [[1, 4], [4, 5], [6, 4], [5, 5], [10, 0], [0, 1], [7, 3], [6, 0], [10, 0], [2, 8, 6]]
-# it 'check record_per_frame' do
-#   result = [5, 14, 29, 29, 60, 61, 77, 97, 117, 133]
-#   game.record_per_frame
-#   expect(game.result).to eq result
-# end
